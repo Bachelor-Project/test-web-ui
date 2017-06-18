a8bfeac879bbab174fdd5731a0085b7de7e434fe
davit-qobuladze <dkobu11@freeuni.edu.ge>
2017-06-18 22:38:00 +0400

    Topic content

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import ModalParent from './modal';
 import TaskUploadTester from './upload/taskUploader';
 import TopicUploaderTest from './upload/topicUploader';
 import AppRouter from './router/testTornadoesRouter';
+import TopicDep from './departments/topic/TopicDep';
+import SideMenu from './sidemenu';
 
 
 // ReactDOM.render(<AppRouter />, document.getElementById('root'));
 
 // ReactDOM.render(<WriteSolution />, document.getElementById('root'));
 // ReactDOM.render(<ModalParent />, document.getElementById('root'));
-ReactDOM.render(<TopicUploaderTest />, document.getElementById('root'));
+// ReactDOM.render(<TopicUploaderTest />, document.getElementById('root'));
+ReactDOM.render(<TopicDep />, document.getElementById('root'));
 
 registerServiceWorker();
